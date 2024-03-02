6b6a29ce62a2375a4329b74243277a9aac13e00c
Surya <suryat2020@gmail.com>
2024-03-02 11:33:38 -0500

    Added MongoDB server for storing chart related data.

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 // Budget API
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const budget = require('./budgetData.json');
+const budgetModel = require('./Models/budgetData');
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use('/', express.static("public"));
 
-app.get("/budget", (req, res) => {
-    res.json(budget);
+const MONGODB_URI = "mongodb://localhost:27017/budgetApp";
+
+mongoose.connect(MONGODB_URI)
+.then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Unable to connect to Database.\n", err);
+})
+
+app.get("/expenses", async (req, res) => {
+    await budgetModel.find().then((data) => {
+        res.json(data);
+    }).catch((connectionError) => {
+        console.error(connectionError);
+        res.status(400).json({error:'Internal Server Error'})
+    });
+})
+
+app.post('/expenses', async (req, res) => {
+    const newItem = new budgetModel(req.body);
+    await newItem.save().then((data) => {
+        res.json(data);
+    }).catch((connectionError) => {
+        console.error(connectionError);
+        res.status(400).json({error: connectionError.message})
+    });
 })
 
 app.listen(port, () => {
